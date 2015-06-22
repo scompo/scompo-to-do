1a6e29774bff56162114d6f049c4608c955719ee
Mauro Scomparin <scompo@gmail.com>
2015-06-22 19:10:25 +0200

    ToDoList stubs

---
+++ src/main/java/com/github/scompo/scompotodo/domain/todolist/ToDoList.java
@@ -0,0 +1,7 @@
+package com.github.scompo.scompotodo.domain.todolist;
+
+import java.io.Serializable;
+
+public class ToDoList implements Serializable{
+
+}
---
+++ src/main/java/com/github/scompo/scompotodo/service/todolist/TodoListService.java
@@ -0,0 +1,8 @@
+package com.github.scompo.scompotodo.service.todolist;
+
+import com.github.scompo.commons.CrudService;
+import com.github.scompo.scompotodo.domain.todolist.ToDoList;
+
+public interface TodoListService extends CrudService<ToDoList, Long>{
+
+}
