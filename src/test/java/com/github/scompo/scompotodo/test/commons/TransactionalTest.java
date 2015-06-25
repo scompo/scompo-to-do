f5c6e7c46000b2acd18bed4e667555143e53cb8e
Mauro Scomparin <scompo@gmail.com>
2015-06-25 17:48:38 +0200

    uhaaaaaaaaaaaaaaaaaaa

--- src/test/java/com/github/scompo/scompotodo/test/commons/TransactionalTest.java
+++ src/test/java/com/github/scompo/scompotodo/test/commons/TransactionalTest.java
@@ -5,8 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +15,7 @@ import com.github.scompo.scompotodo.Application;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
+@IntegrationTest
 @Transactional(propagation = Propagation.REQUIRED)
 public @interface TransactionalTest {
 
