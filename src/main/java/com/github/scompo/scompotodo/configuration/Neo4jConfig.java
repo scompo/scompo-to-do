869fa77076c6a6723b901b781ce12e40434fe885
Mauro Scomparin <scompo@gmail.com>
2015-06-25 14:33:15 +0200

    like this?

--- src/main/java/com/github/scompo/scompotodo/configuration/Neo4jConfig.java
+++ src/main/java/com/github/scompo/scompotodo/configuration/Neo4jConfig.java
@@ -4,12 +4,15 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 
 @Configuration
+@EnableNeo4jRepositories(basePackages = "com.github.scompo.scompoToDo.domain")
 public class Neo4jConfig extends Neo4jConfiguration {
-
-	{
+	
+	public Neo4jConfig(){
+		
 		setBasePackage("com.github.scompo.scompoToDo");
 	}
 
