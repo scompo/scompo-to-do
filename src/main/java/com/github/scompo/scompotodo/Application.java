f169ceaf03da9a71d73b4a16012d59e2ca9adb8b
Mauro Scomparin <scompo@gmail.com>
2015-06-25 14:49:55 +0200

    mh..

--- src/main/java/com/github/scompo/scompotodo/Application.java
+++ src/main/java/com/github/scompo/scompotodo/Application.java
@@ -1,14 +1,34 @@
 package com.github.scompo.scompotodo;
 
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.config.Neo4jConfiguration;
 
 @SpringBootApplication
-public class Application{
+@EnableNeo4jRepositories(basePackages = "com.github.scompo.scompoToDo.domain")
+public class Application extends Neo4jConfiguration {
+	
+	public Application(){
+
+		setBasePackage("com.github.scompo.scompoToDo");
+		System.err.println("set base packages");
+	}
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean(destroyMethod = "shutdown")
+	public GraphDatabaseService graphDatabaseService() {
+
+		System.err.println("creating graph service");
+		return new GraphDatabaseFactory()
+				.newEmbeddedDatabase("scompo-to-do.db");
 	}
 
 }
--- src/main/java/com/github/scompo/scompotodo/configuration/Neo4jConfig.java
+++ src/main/java/com/github/scompo/scompotodo/configuration/Neo4jConfig.java
@@ -1,27 +1,12 @@
 package com.github.scompo.scompotodo.configuration;
 
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
-import org.springframework.data.neo4j.config.Neo4jConfiguration;
 
-@Configuration
-@EnableNeo4jRepositories(basePackages = "com.github.scompo.scompoToDo.domain")
-public class Neo4jConfig extends Neo4jConfiguration {
+//@Configuration
+public class Neo4jConfig {
 	
 	public Neo4jConfig(){
 		
-		setBasePackage("com.github.scompo.scompoToDo");
-		System.err.println("set base packages");
 	}
 
-	@Bean(destroyMethod = "shutdown")
-	public GraphDatabaseService graphDatabaseService() {
 
-		System.err.println("creating graph service");
-		return new GraphDatabaseFactory()
-				.newEmbeddedDatabase("scompo-to-do.db");
-	}
 }
