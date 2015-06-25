99ea8cf514eee9d262731254ee1aa4cdab7c2962
Mauro Scomparin <scompo@gmail.com>
2015-06-25 15:40:09 +0200

    mmm

--- src/main/java/com/github/scompo/scompotodo/Application.java
+++ src/main/java/com/github/scompo/scompotodo/Application.java
@@ -1,9 +1,15 @@
 package com.github.scompo.scompotodo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EnableTransactionManagement
 public class Application{
 
 	public static void main(String[] args) {
--- src/main/java/com/github/scompo/scompotodo/configuration/MvcConfig.java
+++ src/main/java/com/github/scompo/scompotodo/configuration/MvcConfig.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter{
 
 	@Override
