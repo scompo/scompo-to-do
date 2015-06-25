359a308714bdafb76eac34d94faba61475f53b2d
Mauro Scomparin <scompo@gmail.com>
2015-06-25 17:44:20 +0200

    bla

--- src/main/java/com/github/scompo/scompotodo/Application.java
+++ src/main/java/com/github/scompo/scompotodo/Application.java
@@ -1,15 +1,9 @@
 package com.github.scompo.scompotodo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@EnableTransactionManagement
+@SpringBootApplication
 public class Application{
 
 	public static void main(String[] args) {
